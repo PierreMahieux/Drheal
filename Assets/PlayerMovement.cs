8c04dff2d1cf7d51491d00762b5eda3324a0190a
Loic-Glem <loic-glem@hotmail.fr>
2020-01-31 23:41:17 +0100

    Player controller créé

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController controller;
+
+    public float speed = 1200f;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("z")){
+
+            Vector3 move = transform.forward;
+            Debug.Log("pouet");
+
+            controller.Move(move * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey("d"))
+        {
+
+            Vector3 move = transform.right;
+            Debug.Log("pouet");
+
+            controller.Move(move * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey("q"))
+        {
+
+            Vector3 move = transform.right;
+            Debug.Log("pouet");
+
+            controller.Move(move * -speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey("s"))
+        {
+
+            Vector3 move = transform.forward;
+            Debug.Log("pouet");
+
+            controller.Move(move * -speed * Time.deltaTime);
+        }
+    }
+}
