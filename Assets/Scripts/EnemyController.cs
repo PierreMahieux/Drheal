d56f0ec66611bcdb956ca6c1735d1c3568ad4953
Gwen-Del <gwe.del56@gmail.com>
2020-02-01 19:15:29 +0100

    enemy stop

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -18,12 +18,23 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        float distance = Vector3.Distance(target.gameObject.transform.position, transform.position);
+
         if (canMove)
         {
             if (agent)
             {
-                agent.destination = target.gameObject.transform.position;
-                //agent.SetDestination(other.gameObject.transform.position);
+                if(distance <= 2.3)
+                {
+                    agent.isStopped = true;
+                    agent.velocity = Vector3.zero;
+                }
+                else
+                {
+                    agent.isStopped = false;
+                    agent.destination = target.gameObject.transform.position;
+                }
             }
             else
             {
@@ -47,6 +58,15 @@ public class EnemyController : MonoBehaviour
         {
             target = null;
             canMove = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            //collision.getcomponent<PlayerHealth>().Hurt();
+            Debug.Log("aie");
         }
     }
 }
