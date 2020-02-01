a249ce502b3a4dac5f0a0aff883710b0f3bda33e
Gwen-Del <gwe.del56@gmail.com>
2020-02-01 01:13:16 +0100

    Ajout script grabobject et piece

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
+    public List<Piece> MesPieces;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,18 @@ public class Inventory : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag=="Piece")
+        {
+            MesPieces.Add(other.GetComponent<Piece>());
+            other.gameObject.SetActive(false);
+            foreach (var item in MesPieces)
+            {
+                Debug.Log(item.Nom);
+            }
+        }
     }
 }
